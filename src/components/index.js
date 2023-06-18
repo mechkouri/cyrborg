38e0511d7de6eed677d229960447918ed1e7a715
mechkouri <saidmechkouri@gmail.com>
2023-06-18 17:27:18 +0100

    create section most popular

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 
 export {default as Container} from './Container/Container'
 export {default as Navitem} from './NavItem/Navitem'
+export {default as Card} from './Card/Card'
+export {default as SectionHeader} from './SectionHeader/SectionHeader'
+export {default as SectionWrapper} from './SectionWrapper/SectionWrapper'
 export { NavitemDropDown as NavitemDropDown} from './NavItem/Navitem'
 
